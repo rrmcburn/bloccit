67390a8b1cc91acd36ee96cc7a23932252ece62b
Ryan McBurney <rrmcburn@gmail.com>
2017-05-01 21:11:53 -0400

    added sponsored posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,18 @@ require 'random_data'
  end
  posts = Post.all
 
+ # Create Sponsored Posts
+ 50.times do
+
+   SponsoredPost.create!(
+     topic:  topics.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph,
+     price: 50
+   )
+ end
+ sponsoredposts = SponsoredPost.all
+
  # Create Comments
 
  100.times do
@@ -42,4 +54,5 @@ require 'random_data'
  puts "Seed finished"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
+ puts "#{SponsoredPost.count} sponsored posts created"
  puts "#{Comment.count} comments created"
