cb84ed9f6158d16f68c00a07c1a7862d395787f6
Ryan McBurney <rrmcburn@gmail.com>
2017-05-18 21:47:41 -0400

    fixed routing errors but still issues with password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,12 @@ class UsersController < ApplicationController
      @user = User.new
   end
 
-  def confirm
-     @user = User.new
-     @user.name = params[:user][:name]
-     @user.email = params[:user][:email]
-     @user.password = params[:user][:password]
-     @user.password_confirmation = params[:user][:password_confirmation]
-   end
-
   def create
      @user = User.new
-     @user.name = params[:user][:name]
-     @user.email = params[:user][:email]
-     @user.password = params[:user][:password]
-     @user.password_confirmation = params[:user][:password_confirmation]
+     @user.name = params['user']['name']
+     @user.email = params['user']['email']
+     @user.password = params['user']['password']
+     @user.password_confirmation = params['user']['password_confirmation']
 
      if @user.save
        flash[:notice] = "Welcome to Bloccit #{@user.name}!"
@@ -27,5 +19,13 @@ class UsersController < ApplicationController
        render :new
      end
    end
+
+   def confirm
+      @user = User.new
+      @user.name = params['user']['name']
+      @user.email = params['user']['email']
+      @user.password = params['user']['password']
+      @user.password_confirmation = params['user']['password_confirmation']
+    end
 
 end
