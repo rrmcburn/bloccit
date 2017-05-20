4689f555d5cf83b0def86e1f95dc49a2c0763d20
Ryan McBurney <rrmcburn@gmail.com>
2017-05-20 12:31:47 -0400

    minor updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates 'password', presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates 'password', length: { minimum: 6 }, allow_blank: true
 
   validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
+             length: { minimum: 2, maximum: 254 }
 
   has_secure_password
 
