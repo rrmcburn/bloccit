c2650b67ca96512cd9409190a53a42766a01e5bc
Ryan McBurney <rrmcburn@gmail.com>
2017-05-30 11:08:50 -0400

    added comment and vote factories and updated User Show view to render favorite posts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def has_posts?
+    current_user.posts.count != 0
+  end
+
+  def display_fav_post(post)
+    username_info = "<h3>#{post.user.name}</h3> <img src='#{post.user.avatar_url}' class='media-object' />"
+    username_info.html_safe
+  end
 end
